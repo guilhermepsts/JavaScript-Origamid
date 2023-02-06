82c6b94496debd82aa39c5e72572d670f4c51ee3
Guilherme Santos <guilhermegps08@gmail.com>
2023-02-06 16:51:34 -0300

    começo de escopo

---
+++ atribuicao-e-ternario/script-aula.js
@@ -0,0 +1,10 @@
+var idade = 15 ;
+var podeBeber = (idade >= 18) ? 'Pode beber!!' : 'NÃO PODE BEBER!!!!';
+console.log(podeBeber)
+
+var possuiFaculdade = true;
+if(possuiFaculdade)
+  console.log('Sim, possui.');
+else
+  console.log('Ainda não.');
+
---
+++ escopo/script.js
@@ -0,0 +1,13 @@
+function mostrarCarro() {
+  var carro = 'Fusca';
+  console.log(carro);
+}
+
+mostrarCarro(); // Fusca no console
+// console.log(carro); // Erro, carro is not defined
+
+for(let i = 0; i < 10; i++) {
+  console.log(`Número ${i}`)
+}
+console.log(i);
+ 
